21ace1de66e918b575c96b1258f42d841d06c60e
Braedan Royer <royerbraedan@gmail.com>
2024-05-22 16:14:35 -0700

    added the package json file, replaced the for of with the forEach loop

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,33 @@ const tweetInput = document.querySelector("#tweet-input")
 tweetBtn.addEventListener("click", () => {
     console.log(tweetInput.value);
 })
+
+function getFeedHTML() {
+    let feedHTML = ''
+    tweetsData.forEach((tweet) => {
+        feedHTML += `
+                <div class="tweet">
+                    <div class="tweet-inner">
+                        <img src=${tweet.profilePic} class="profile-pic">
+                        <div>
+                            <p class="handle">${tweet.handle}</p>
+                            <p class="tweet-text">${tweet.tweetText}</p>
+                            <div class="tweet-details">
+                                <span class="tweet-detail">
+                                    ${tweet.replies.length}
+                                </span>
+                                <span class="tweet-detail">
+                                    ${tweet.likes}
+                                </span>
+                                <span class="tweet-detail">
+                                    ${tweet.retweets}
+                                </span>
+                            </div>   
+                        </div>            
+                    </div>
+                </div>`
+    })
+    return feedHTML
+}
+
+console.log(getFeedHTML())
