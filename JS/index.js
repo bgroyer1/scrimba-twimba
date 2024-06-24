50fcf0f6209ddb23ff48ce34ddfcb2aaffbd4977
Braedan Royer <royerbraedan@gmail.com>
2024-06-24 13:02:43 -0700

    adding dataset features

--- JS/index.js
+++ JS/index.js
@@ -4,8 +4,18 @@ const tweetBtn = document.querySelector("#tweet-btn")
 const tweetInput = document.querySelector("#tweet-input")
 
 tweetBtn.addEventListener("click", () => {
-    console.log(tweetInput.value);
+    console.log(tweetInput.value)
+    tweetInput.value = ''
 })
+
+document.addEventListener("click", (e) => {
+    if (e.target.dataset.like) {
+        console.log(e.target.dataset.like)}
+})
+
+function handleLikeClick() {
+    
+}
 
 function getFeedHTML() {
     let feedHTML = ''
@@ -19,12 +29,16 @@ function getFeedHTML() {
                             <p class="tweet-text">${tweet.tweetText}</p>
                             <div class="tweet-details">
                                 <span class="tweet-detail">
+                                    <i class="fa-regular fa-comment-dots"
+                                    data-reply="${tweet.uuid}></i>
                                     ${tweet.replies.length}
                                 </span>
                                 <span class="tweet-detail">
+                                    <i class="fa-regular fa-heart" data-likes="${tweet.uuid}"></i>
                                     ${tweet.likes}
                                 </span>
                                 <span class="tweet-detail">
+                                    <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
                                     ${tweet.retweets}
                                 </span>
                             </div>   
@@ -35,4 +49,9 @@ function getFeedHTML() {
     return feedHTML
 }
 
-console.log(getFeedHTML())
+function renderFeed() {
+    document.querySelector("#feed").innerHTML = getFeedHTML()
+}
+
+renderFeed()
+
